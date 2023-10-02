c0d328c9766f571cac5c071d72b765103354e20f
Justin Leahy <justinjleahy@gmail.com>
2023-10-02 09:46:54 -0400

    Worked on database SQL schema files

---
+++ backend/sql/01-create-schema.sql
@@ -0,0 +1,35 @@
+-- User table
+CREATE TABLE user {
+    id SERIAL PRIMARY KEY,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    user_name text NOT NULL,
+    user_uuid binary(16) NOT NULL
+}
+
+-- Account Type enum
+CREATE TYPE account_type AS ENUM {
+    'checking',
+    'savings'
+}
+
+-- Compound Interest Unit enum
+CREATE TYPE compound_interest_unit AS ENUM {
+    'day',
+    'week',
+    'month',
+    'year'
+}
+
+-- Account table
+CREATE TABLE account {
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES user(id),
+    account_type account_type NOT NULL,
+    account_nickname text NOT NULL,
+    account_uuid binary(16) NOT NULL,
+    interest_whole INT8 NOT NULL,
+    interest_decimal INT8 NOT NULL,
+    interest_decimal_exponent INT8 NOT NULL,
+    compound_interest_value INT NOT NULL,
+}
