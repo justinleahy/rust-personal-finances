15c18f0ae23738b1327e0d6c89c6c01a3b42d9e9
Justin Leahy <justinjleahy@gmail.com>
2023-10-05 15:12:40 -0400

    Created user type

--- backend/src/model/finances.rs
+++ backend/src/model/finances.rs
@@ -3,3 +3,18 @@ use sqlb::{Fields, HasFields, SqlBuilder};
 use super::{db::Db, reference::{USER_MAC_TABLE, USER_MAC_COLUMNS, ACCOUNT_MAC_TABLE, ACCOUNT_MAC_COLUMNS}};
 use crate::model;
 
+// region: Finance types
+
+// region: Users type
+
+#[derive(sqlx::FromRow, Debug, Clone)]
+pub struct User {
+    pub id: Uuid,
+    pub username: String,
+    pub first_name: String,
+    pub last_name: String,
+}
+
+// endregion: Users type
+
+// endregion: Finance types
