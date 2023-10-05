63a7c633fe73076761224b7ffc9d738f39023379
Justin Leahy <justinjleahy@gmail.com>
2023-10-05 15:29:34 -0400

    Created UserPatch type

--- backend/src/model/finances.rs
+++ backend/src/model/finances.rs
@@ -15,6 +15,13 @@ pub struct User {
     pub last_name: String,
 }
 
+#[derive(sqlb::Fields, Clone)]
+pub struct UserPatch {
+    pub username: Option<String>,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>
+}
+
 // endregion: Users type
 
 // endregion: Finance types
