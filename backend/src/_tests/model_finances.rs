5cb2749211bbb8b2a7dd1d80faa6c0391bf6ed2a
Justin Leahy <justinjleahy@gmail.com>
2023-10-06 13:31:17 -0400

    Moving from chrono crate to time crate

--- backend/src/_tests/model_finances.rs
+++ backend/src/_tests/model_finances.rs
@@ -1,5 +1,6 @@
 use std::ptr::null;
 use uuid::uuid;
+use time::Date;
 use crate::model::db::init_db;
 use crate::model::finances::{UserMac, AccountMac, TransactionMac};
 use crate::model::finances::{AccountTypes, InterestFrequencyUnits};
--- backend/src/model/finances.rs
+++ backend/src/model/finances.rs
@@ -1,4 +1,4 @@
-use sqlx::types::time::Date;
+use time::Date;
 use uuid::{uuid, Uuid};
 use sqlb::{Fields, HasFields, SqlBuilder};
 use super::{db::Db, reference::{USER_MAC_TABLE, USER_MAC_COLUMNS, ACCOUNT_MAC_TABLE, ACCOUNT_MAC_COLUMNS, TRANSACTION_MAC_TABLE, TRANSACTION_MAC_COLUMNS}};
