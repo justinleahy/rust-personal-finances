a71676abd1397e6dd30bf0011cc3e7c6bb242b38
Justin Leahy <justinjleahy@gmail.com>
2023-10-07 18:55:49 -0400

    Created Error enum

--- backend/src/web/mod.rs
+++ backend/src/web/mod.rs
@@ -2,3 +2,9 @@ use crate::model::Db;
 use std::path::Path;
 use std::sync::Arc;
 use warp::Filter;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Web server failed to start because web-folder '{0}' not found.")]
+    FailStartWebFolderNotFound(String)
+}
