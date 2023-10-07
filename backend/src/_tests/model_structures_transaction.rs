b0074b2286adf5109a4dd7b29e94dc68ecdcdca7
Justin Leahy <justinjleahy@gmail.com>
2023-10-07 18:12:57 -0400

    Created TransactionMac::create test

--- backend/src/_tests/model_structures_transaction.rs
+++ backend/src/_tests/model_structures_transaction.rs
@@ -3,7 +3,40 @@ use time::macros::datetime;
 use uuid::uuid;
 use time::OffsetDateTime;
 use crate::model::db::init_db;
-use crate::model::structures::transaction::{TransactionMac, TransactionTypes, TransactionCategories};
+use crate::model::structures::transaction::{TransactionTypes, TransactionCategories, TransactionPatch, TransactionMac};
+
+#[tokio::test]
+async fn transactionmac_create() -> Result<(), Box<dyn std::error::Error>> {
+    // Fixture
+    let db = init_db().await?;
+    let account_id = uuid!("00000000-0000-0000-0000-000000000001");
+    let transaction_date = OffsetDateTime::now_utc();
+    let transaction_fx = TransactionPatch {
+        account_id: Some(account_id),
+        transaction_date: Some(transaction_date),
+        transaction_type: Some(TransactionTypes::Expense),
+        category: Some(TransactionCategories::Expense),
+        transaction_integer: Some(1),
+        transaction_decimal: Some(2278),
+        transaction_exponent: Some(2),
+        comment: Some("Test Transaction".to_string())
+    };
+    
+    // Action
+    let transaction_created = TransactionMac::create(&db, transaction_fx.clone()).await?;
+
+    // Check
+    assert_eq!(account_id, transaction_created.account_id);
+    assert_eq!(transaction_date.date(), transaction_created.transaction_date.date());
+    assert_eq!(TransactionTypes::Expense, transaction_created.transaction_type);
+    assert_eq!(TransactionCategories::Expense, transaction_created.category);
+    assert_eq!(1, transaction_created.transaction_integer);
+    assert_eq!(2278, transaction_created.transaction_decimal);
+    assert_eq!(2, transaction_created.transaction_exponent);
+    assert_eq!("Test Transaction", transaction_created.comment);
+    
+    Ok(())
+}
 
 #[tokio::test]
 async fn transactionmac_list() -> Result<(), Box<dyn std::error::Error>> {
