c1b6b9bd7eba4cfc862a6fd19dc59b3376a5b197
Justin Leahy <justinjleahy@gmail.com>
2023-10-07 18:56:48 -0400

    Updated main function to start the web server with index.html as root

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,32 @@
 #![allow(unused)]
 
+use model::init_db;
+use std::env;
+use std::sync::Arc;
+use web::start_web;
+
 mod model;
 mod security;
 mod web;
 
-fn main() {
-    println!("Hello, world!");
+const DEFAULT_WEB_FOLDER: &'static str = "web-folder/";
+const DEFAULT_WEB_PORT: u16 = 8080;
+
+#[tokio::main]
+async fn main() {
+    // Compute web_folder
+    let mut args: Vec<String> = env::args().collect();
+    let web_folder = args.pop().unwrap_or_else(|| DEFAULT_WEB_FOLDER.to_string());
+    let web_port = DEFAULT_WEB_PORT;
+
+    // Get the database
+    // TODO - Loop until valid DB
+    let db = init_db().await.expect("connect init db");
+    let db = Arc::new(db);
+
+    // Start the server
+    match start_web(&web_folder, web_port, db).await {
+        Ok(_) => println!("Server ended"),
+        Err(ex) => println!("ERROR - Web server failed to start. Cause {:?}", ex),
+    }
 }
