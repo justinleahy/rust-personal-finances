71ab1cd5ede09d0f9c8c27f87928a0d3c7e1a239
Justin Leahy <justinjleahy@gmail.com>
2023-10-08 15:38:38 -0400

    Created /api/user get, create, update, and delete endpoints

--- backend/src/web/user.rs
+++ backend/src/web/user.rs
@@ -1,5 +1,6 @@
 use crate::model::Db;
 use crate::model::UserMac;
+use crate::model::UserPatch;
 use std::convert::Infallible;
 use std::sync::Arc;
 use serde_json::json;
@@ -7,25 +8,75 @@ use warp::Filter;
 use warp::reply::Json;
 use super::json_response;
 use super::with_db;
+use uuid::Uuid;
 
 pub fn user_rest_filters(base_path: &'static str, db: Arc<Db>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     // /api/user
-    let transaction_path = warp::path(base_path).and(warp::path("user"));
+    let user_path = warp::path(base_path).and(warp::path("user"));
     let common = with_db(db.clone());
 
-    let list = transaction_path
+    // LIST users `GET /api/user`
+    let list = user_path
         .and(warp::get())
         .and(warp::path::end())
         .and(common.clone())
         .and_then(user_list);
 
-    list
+    // GET user `GET /api/user/{id}`
+    let get = user_path
+        .and(warp::get())
+        .and(common.clone())
+        .and(warp::path::param())
+        .and_then(user_get);
+
+    // CREATE user `POST /api/user with body UserPatch`
+    let create = user_path
+        .and(warp::post())
+        .and(common.clone())
+        .and(warp::body::json())
+        .and_then(user_create);
+
+    // UPDATE user `PATCH /api/user/{id} with body UserPatch`
+    let update = user_path
+        .and(warp::patch())
+        .and(common.clone())
+        .and(warp::path::param())
+        .and(warp::body::json())
+        .and_then(user_update);
+
+    // DELETE user `DELETE /api/user/{id}`
+    let delete = user_path
+        .and(warp::delete())
+        .and(common.clone())
+        .and(warp::path::param())
+        .and_then(user_delete);
+
+    list.or(get).or(create).or(update).or(delete)
 }
 
 async fn user_list(db: Arc<Db>) -> Result<Json, warp::Rejection> {
-    // TODO: Add proper error handling
-    let users = UserMac::list(&db).await.unwrap();
+    let users = UserMac::list(&db).await?;
     json_response(users)
+}
+
+async fn user_get(db: Arc<Db>, id: Uuid) -> Result<Json, warp::Rejection> {
+    let user = UserMac::get(&db, id).await?;
+    json_response(user)
+}
+
+async fn user_create(db: Arc<Db>, patch: UserPatch) -> Result<Json, warp::Rejection> {
+    let user = UserMac::create(&db, patch).await?;
+    json_response(user)
+}
+
+async fn user_update(db: Arc<Db>, id: Uuid, patch: UserPatch) -> Result<Json, warp::Rejection> {
+    let user = UserMac::update(&db, id, patch).await?;
+    json_response(user)
+}
+
+async fn user_delete(db: Arc<Db>, id: Uuid) -> Result<Json, warp::Rejection> {
+    let user = UserMac::delete(&db, id).await?;
+    json_response(user)
 }
 
 #[cfg(test)]
