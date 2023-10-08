eeb148dbb19a1f745d9cd3cead00a0894f21fed5
Justin Leahy <justinjleahy@gmail.com>
2023-10-08 13:28:27 -0400

    Removed unused imports

--- backend/src/model/db.rs
+++ backend/src/model/db.rs
@@ -3,7 +3,7 @@ use sqlx::{Pool, Postgres};
 use std::fs;
 use std::path::PathBuf;
 use std::time::Duration;
-use super::reference::{PG_HOST, PG_ROOT_DB, PG_ROOT_USER, PG_ROOT_PASSWORD, PG_APP_DB, PG_APP_USER, PG_APP_PASSWORD, PG_APP_MAX_CONNECTIONS, SQL_DIR, SQL_RECREATE};
+use super::reference::{PG_HOST, PG_ROOT_DB, PG_ROOT_USER, PG_ROOT_PASSWORD, PG_APP_DB, PG_APP_USER, PG_APP_PASSWORD, PG_APP_MAX_CONNECTIONS};
 
 pub type Db = Pool<Postgres>;
 
