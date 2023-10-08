4625f6e007070de9f243f3f0aea96ca418e1efb3
Justin Leahy <justinjleahy@gmail.com>
2023-10-08 11:44:33 -0400

    New Function: user_context_from_token

--- backend/src/security/mod.rs
+++ backend/src/security/mod.rs
@@ -4,6 +4,15 @@ pub struct UserContext {
 	pub user_id: String
 }
 
+pub async fn user_context_from_token(token: &str) -> Result<UserContext, Error> {
+	// TODO: Real validation needed
+	match token.parse::<String>() {
+		Ok(user_id) => Ok(UserContext { user_id }),
+		Err(_) => Err(Error::InvalidToken((token.to_string())))
+	}
+
+}
+
 #[derive(ThisError, Debug)]
 pub enum Error {
 	#[error("Invalid Token {0}")]
