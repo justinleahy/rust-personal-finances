6fc33aff8c2ce61597066f103e6b50428c490252
Justin Leahy <justinjleahy@gmail.com>
2023-10-08 23:23:21 -0400

    Created /api/account get endpoint test

--- backend/src/_tests/web_account.rs
+++ backend/src/_tests/web_account.rs
@@ -68,6 +68,43 @@ async fn account_get() -> Result<()> {
     Ok(())
 }
 
+#[tokio::test]
+async fn account_create() -> Result<()> {
+    // Fixture
+    let db = init_db().await?;
+    let db = Arc::new(db);
+    let account_apis = account_rest_filters("api", db).recover(handle_rejection);
+    let body = json!({
+        "user_id": "00000000-0000-0000-0000-000000000000",
+        "account_type": "Savings",
+        "nickname": "Ally Savings",
+        "interest": 0.0456,
+        "interest_frequency": 1,
+        "interest_frequency_unit": "Day"
+    });
+
+    // Action
+    let resp = warp::test::request()
+        .method("POST")
+        .path("/api/account")
+        .json(&body)
+        .reply(&account_apis)
+        .await;
+
+    // Check
+    assert_eq!(200, resp.status(), "http status");
+
+    let account: Account = extract_body_data(resp)?;
+
+    assert_eq!(AccountTypes::Savings, account.account_type);
+    assert_eq!("Ally Savings", account.nickname);
+    assert_eq!(0.0456, account.interest);
+    assert_eq!(1, account.interest_frequency);
+    assert_eq!(InterestFrequencyUnits::Day, account.interest_frequency_unit);
+
+    Ok(())
+}
+
 fn extract_body_data<D>(resp: Response<Bytes>) -> Result<D> where for <'de> D: Deserialize<'de> {
     // Parse the body as serde_json::Value
     let body = from_utf8(resp.body())?;
