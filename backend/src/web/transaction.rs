100de3ce5727ed78d660b86658933482807512a9
Justin Leahy <justinjleahy@gmail.com>
2023-10-11 00:21:58 -0400

    Converting transaction endpoints to /api/account/{id}/transaction

--- backend/src/web/transaction.rs
+++ backend/src/web/transaction.rs
@@ -12,24 +12,25 @@ use uuid::Uuid;
 
 pub fn transaction_rest_filters(base_path: &'static str, db: Arc<Db>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     // /api/transaction
-    let transaction_path = warp::path(base_path).and(warp::path("transaction"));
+    //let transaction_path = warp::path(base_path).and(warp::path("account")).and(warp::path::param::<Uuid>());
+    let transaction_path = warp::path!("api" / "account" / Uuid / "transaction");
     let common = with_db(db.clone());
 
-    // LIST transactions `GET /api/transaction`
+    // LIST transactions `GET /api/account{id}/transaction/{id}`
     let list = transaction_path
         .and(warp::get())
-        .and(warp::path::end())
         .and(common.clone())
+        .and(warp::path::end())
         .and_then(transaction_list);
 
-    // GET transaction `GET /api/transaction/{id}`
+    // GET transaction `GET /api/account/{id}/transaction/{id}`
     let get = transaction_path
         .and(warp::get())
         .and(common.clone())
-        .and(warp::path::param())
+        .and(warp::path::param::<Uuid>())
         .and_then(transaction_get);
 
-    // CREATE transaction `POST /api/transaction with body TransactionPatch`
+    // CREATE transaction `POST /api/account/{id}/transaction with body TransactionPatch`
     let create = transaction_path
         .and(warp::post())
         .and(common.clone())
@@ -44,6 +45,7 @@ pub fn transaction_rest_filters(base_path: &'static str, db: Arc<Db>) -> impl Fi
         .and(warp::body::json())
         .and_then(transaction_update);
 
+    /*
     // DELETE transaction `DELETE /api/transaction/{id}`
     let delete = transaction_path
         .and(warp::delete())
@@ -52,24 +54,26 @@ pub fn transaction_rest_filters(base_path: &'static str, db: Arc<Db>) -> impl Fi
         .and_then(transaction_delete);
 
     list.or(get).or(create).or(update).or(delete)
+    */
+    list.or(get).or(create).or(update)
 }
 
-async fn transaction_list(db: Arc<Db>) -> Result<Json, warp::Rejection> {
+async fn transaction_list(account_id: Uuid, db: Arc<Db>) -> Result<Json, warp::Rejection> {
     let transactions = TransactionMac::list(&db).await?;
     json_response(transactions)
 }
 
-async fn transaction_get(db: Arc<Db>, id: Uuid) -> Result<Json, warp::Rejection> {
-    let transaction = TransactionMac::get(&db, id).await?;
+async fn transaction_get(account_id: Uuid, db: Arc<Db>, transaction_id: Uuid) -> Result<Json, warp::Rejection> {
+    let transaction = TransactionMac::get(&db, transaction_id).await?;
     json_response(transaction)
 }
 
-async fn transaction_create(db: Arc<Db>, patch: TransactionPatch) -> Result<Json, warp::Rejection> {
+async fn transaction_create(account_id: Uuid, db: Arc<Db>, patch: TransactionPatch) -> Result<Json, warp::Rejection> {
     let transaction = TransactionMac::create(&db, patch).await?;
     json_response(transaction)
 }
 
-async fn transaction_update(db: Arc<Db>, id: Uuid, patch: TransactionPatch) -> Result<Json, warp::Rejection> {
+async fn transaction_update(account_id: Uuid, db: Arc<Db>, id: Uuid, patch: TransactionPatch) -> Result<Json, warp::Rejection> {
     let transaction = TransactionMac::update(&db, id, patch).await?;
     json_response(transaction)
 }
